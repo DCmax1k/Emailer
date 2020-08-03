1d86ba0d9d0d374c4354e92475b40f16a035925a
Dylan Caldwell <dylan.caldwell35@gmail.com>
2020-08-02 21:55:00 -0400

    test

--- server.js
+++ server.js
@@ -19,14 +19,6 @@ const transporter = nodemailer.createTransport({
 });
 
 app.post('/portfolio/contact', (req, res) => {
-  // Develope return value
-  let messageArr = JSON.stringify(req.body.message).split('');
-  let messageTrim = messageArr.trim();
-  let messagePop = messageTrim.pop();
-  let messageShift = messagePop.shift();
-  let messageJoin = messageShift.join('');
-  let newMessage = messageJoin.replace(/\\r\\n/g, '<br />');
-  // Mail Options
   const mailOptions = {
     from: 'Contact Form',
     to: 'dylan.caldwell35@gmail.com',
@@ -45,7 +37,7 @@ app.post('/portfolio/contact', (req, res) => {
         <br />
         <b>Message:</b>
         <br />
-        ${newMessage}
+        ${req.body.message}
         <hr />
         `,
   };
