438c0aafa3d9a6b8d6aa6b9ee89f69fd5c4e19e3
Dylan Caldwell <dylancaldwell@Dylans-iMac.lan>
2020-12-30 02:35:55 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## About the Project!
+
+This project is used for other projects to contact this RESTAPI to be able to notify myself.
--- server.js
+++ server.js
@@ -3,23 +3,24 @@ const app = express();
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
+// GOOGLEAPIS
+const { google } = require('googleapis');
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'noreplydevapp@gmail.com',
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
 
-app.post('/portfolio/contact', (req, res) => {
-  // Adding return value
+app.post('/portfolio/contact', async (req, res) => {
+  try {
+// Adding return value
   let newMessageArr = JSON.stringify(req.body.message)
     .replace(/\\r\\n/gi, '<br />')
     .trim()
@@ -28,9 +29,23 @@ app.post('/portfolio/contact', (req, res) => {
   newMessageArr.shift();
   let newMessage = newMessageArr.join('');
   // Mail Options
+  const accessToken = await oAuth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'noreplydevapp@gmail.com',
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      refreshToken: REFRESH_TOKEN,
+      accessToken: accessToken,
+    }
+  })
+
   const mailOptions = {
     from: 'Contact Form',
-    to: 'dylan.caldwell35@gmail.com',
+    to: process.env.PERSONAL_EMAIL,
     subject: 'Contact Form',
     html: `
         <hr />
@@ -54,6 +69,10 @@ app.post('/portfolio/contact', (req, res) => {
     if (err) return console.error(err);
   });
   res.redirect('https://www.dylancaldwell.tk/thanks');
+  } catch(err) {
+    console.error(err);
+  }
+  
 });
 
 app.listen(process.env.PORT || 3000);
